2e11da96d3b5fbefc8a3b1d89a8e7bac8dae6d93
Sofía Isoardi Prieto <sofia1_chofi@hotmail.com>
2025-01-18 15:34:14 -0300

    Insercion de Entidades y dbContext

---
+++ BackEnd/EduLink.Entidades/EntidadesDBB/Ciudad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduLink.Entidades.EntidadesBDD
+{
+    public class Ciudad
+    {
+        public int CiudadId { get; set; }
+        public string NombreCiudad { get; set; }
+
+        public string ProvinciaId { get; set; }
+        public Provincia Provincia { get; set; }
+
+        public ICollection<Estudiante> Estudiantes{ get; set; }
+    }
+}
---
+++ BackEnd/EduLink.Entidades/EntidadesDBB/EstudianteExamen.cs
@@ -0,0 +1,17 @@
+﻿using EduLink.Entidades.Enums;
+
+namespace EduLink.Entidades.EntidadesBDD
+{
+    public class EstudianteExamen
+    {
+
+        public int EstudianteExamenId { get; set; }
+        public int EstudianteId { get; set; }
+        public int ExamenId { get; set; }
+        public double Nota { get; set; }
+        public EstadoExamenEnum EstadoExamen { get; set; }
+
+        public Estudiante Estudiante { get; set; } // Propiedad de navegación
+        public Examen Examen { get; set; } // Propiedad de navegación
+    }
+}
